cb262eb2b07ca6f2aba223d2a99b87a5ec804400
Nourkhalfaoui <nour.khalfaoui@esprit.tn>
2024-07-18 21:17:35 +0100

    voteProcess

---
+++ src/main/java/com/example/manajeroback/services/SessionService.java
@@ -0,0 +1,31 @@
+package com.example.manajeroback.services;
+
+import com.example.manajeroback.entities.News;
+import com.example.manajeroback.entities.Session;
+import com.example.manajeroback.repositories.NewsRepository;
+import com.example.manajeroback.repositories.SessionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class SessionService {
+    SessionRepository sessionRepository ;
+
+    public Session addSession(Session session) {
+        return sessionRepository.save(session);
+    }
+
+    public List<Session> getAllSessions() {
+        return sessionRepository.findAll();
+    }
+
+    public Session getSessionById(String id) {
+        return sessionRepository.findById(id).orElse(null);
+    }
+    public void deleteSession(String id) {
+        sessionRepository.deleteById(id);
+    }
+
+}
